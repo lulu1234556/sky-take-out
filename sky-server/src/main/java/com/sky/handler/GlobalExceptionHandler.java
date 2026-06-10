eec5f2f8bcbc410cfa008a3eb4c8bc92be56640e
tst <2175797540@qq.com>
2026-06-10 15:02:11 +0800

    前端异常显示

--- sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
+++ sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.sky.handler;
 
 import com.sky.constant.MessageConstant;
+import com.sky.exception.BaseException;
 import com.sky.result.Result;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -22,5 +23,11 @@ public class GlobalExceptionHandler {
             return Result.error(name+ MessageConstant.ALREADY_EXIST);
         }
         return Result.error(MessageConstant.UNKNOWN_ERROR);
+    }
+
+    @ExceptionHandler(BaseException.class)
+    public Result exceptionHandler(BaseException ex){
+        log.info("业务异常信息：{}", ex.getMessage());
+        return Result.error(ex.getMessage());
     }
 }
