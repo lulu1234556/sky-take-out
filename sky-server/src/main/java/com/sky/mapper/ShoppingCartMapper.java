14fcbd1d270bcd5461c257145e1160a29ef7debc
tst <2175797540@qq.com>
2026-06-13 09:12:44 +0800

    购物车

---
+++ sky-server/src/main/java/com/sky/mapper/ShoppingCartMapper.java
@@ -0,0 +1,27 @@
+package com.sky.mapper;
+
+import com.sky.annotation.AutoFill;
+import com.sky.context.BaseContext;
+import com.sky.entity.ShoppingCart;
+import com.sky.enumeration.OperationType;
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Update;
+
+import java.util.List;
+
+public interface ShoppingCartMapper {
+    List<ShoppingCart> list(ShoppingCart shoppingCart);
+
+    @AutoFill(OperationType.UPDATE)
+    @Update("update shopping_cart set number=#{number} where id=#{id} ")
+    void updateNumber(ShoppingCart shoppingCart);
+
+    void insert(ShoppingCart shoppingCart);
+
+    @Delete("delete from shopping_cart where user_id=#{id}")
+    void clean(Long userId);
+
+
+    @Delete("delete from shopping_cart where id=#{id}")
+    void delete(Long id);
+}
